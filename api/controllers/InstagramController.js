ac61ddd04756cdf98208889ff4cf97b710e89ffa
Pedro Pablo Miquel <Pedro Pablo Miquel>
2016-05-15 16:02:44 -0400

    Travis

--- api/controllers/InstagramController.js
+++ api/controllers/InstagramController.js
@@ -5,10 +5,10 @@ var ig = require('instagram-node').instagram();
 
 module.exports = {
 
-getTagInfo: function (tag,token){
+getTagInfo: function (tag){
 	
 	return new promise(function(resolve,reject){
-		ig.use({ access_token: [token]});
+		ig.use({ access_token: '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'});
 		ig.tag(tag.toString(),function(err,result,remaining,limit){var cantidad = result["media_count"]
 		resolve(cantidad)
 	});
@@ -16,10 +16,10 @@ getTagInfo: function (tag,token){
 
 },
 
-getTagMedia: function(tag,token){
+getTagMedia: function(tag){
 
 	return new promise(function(resolve,reject){
-		ig.use({ access_token: [token]});
+		ig.use({ access_token: '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'});
 		ig.tag_media_recent(tag.toString(), function(err, medias, pagination, remaining, limit) {
 			var posts = [];
 			for (var i = 0; i < Object.key(medias).lenght;i++){
@@ -46,16 +46,16 @@ getTagMedia: function(tag,token){
 getTag: function (req,res){
 	var self = this;
 	var tag = req.body.tag;
-	var token = req.body.token;
+	var access_token = req.body.access_token;
 	if(!tag)
   		return res.badRequest('Falta el tag');
-  	if(!token)
-  		return res.badRequest('Falta el token de autorización');
+  	//if(!access_token)
+  	//	return res.badRequest('Falta el token de autorización');
   	var response = {};
-  	self.getTagInfo(tag,token)
+  	self.getTagInfo(tag)
   	.then(function(cantidad){
   		response.metadata = {total: cantidad};
-  		return self.getTagMedia(tag,token);	
+  		return self.getTagMedia(tag);	
   	})
   	.then(function(posts){
   		response.posts = posts;
